a4be0d2ee1ade98712aa86d3d77c42f25251748c
Vitor Andrade <vitorands@gmail.com>
2020-04-03 16:23:01 -0300

    back-end da app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+//JSX (JavaScript XML)
+
+function App() {
+  //Array [valor, funcaoDeAtualizacao]
+  return (
+    <Routes />
+  );
+}
+
+export default App;
